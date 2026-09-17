0958f17279b1c17d144ce238b34e9b434f5b51f5
Txinto Vaz <>
2026-09-17 03:49:17 +0100

    chore(release): declare cosmosys 0.1.3

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :cosmosys do
   name 'cosmoSys'
   author 'cosmoBots.eu'
   description 'Base plugin for cosmoSys on top of Redmine.'
-  version '0.1.2'
+  version '0.1.3'
   url 'https://github.com/cosmoBots/cosmoSys'
   author_url 'https://cosmobots.eu'
 
